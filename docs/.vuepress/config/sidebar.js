e5d5a1cf3885b5bcf2a1485d2e267e2596c36037
eyesYeager <eyesyeager@163.com>
2023-08-20 01:42:20 +0800

    添加系统设计内容

--- docs/.vuepress/config/sidebar.js
+++ docs/.vuepress/config/sidebar.js
@@ -4,6 +4,12 @@ module.exports = {
     ],
     '/design/': [
         ['', '前言'],
+        ['topK', 'TopK问题'],
+        ['single-array-frequency-sort', '单数组频度排序'],
+        ['massive-array-frequency-sort', '多数组频度排序'],
+        ['find-mid-num', '查找中位数'],
+        ['find-same-url', '查找相同的url'],
+        ['count-phone-num', '统计不同号码的个数'],
     ],
     '/experience/': [
         ['', '前言'],
