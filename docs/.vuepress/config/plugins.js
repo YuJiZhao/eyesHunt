bd34853757b5588b99febbd9cd31e3ac81a3fd12
eyesYeager <eyesyeager@163.com>
2023-08-19 18:11:36 +0800

    添加插件

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = []
+module.exports = [
+    // 回到顶部
+    // @{link: https://vuepress.vuejs.org/zh/plugin/official/plugin-back-to-top.html}
+    '@vuepress/back-to-top',
+    // 加载进度条
+    // @{link: https://vuepress.vuejs.org/zh/plugin/official/plugin-nprogress.html}
+    '@vuepress/nprogress',
+]
